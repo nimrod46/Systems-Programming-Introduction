acd706d65d1f801a753ad5c91b56cacc15c3e4b6
nimrod46 <nimrodma99@gmail.com>
2022-04-07 15:09:43 +0300

    Finished main HW3 functionality

--- HW3.c
+++ HW3.c
@@ -1,11 +1,14 @@
+//
+// Created by nimrod on 05-Apr-22.
+//
 #include <stdio.h>
 #include <stdbool.h>
 #include <ctype.h>
 #include <malloc.h>
 
-//
-// Created by nimrod on 05-Apr-22.
-//
+void print_file_info(const char *name, unsigned int lines_count, unsigned int words_count,
+                     unsigned int char_count);
+
 unsigned int my_strlen(const char str[]) {
     unsigned int size = 0;
     while (str[size] != '\0') {
@@ -13,14 +16,12 @@ unsigned int my_strlen(const char str[]) {
     }
 
     return size;
-
-
 }
 
 void my_strcat(char dest[], const char src[]) {
     unsigned int dest_size = my_strlen(dest);
     unsigned int src_size = my_strlen(src);
-    for (int i = 0; i < src_size; ++i) {
+    for (int i = 0; i <= src_size; ++i) {
         dest[dest_size + i] = src[i];
     }
 }
@@ -76,7 +77,7 @@ char *get_chapter_file_name(const char s[], const char prefix[], const char suff
     return title;
 }
 
-int main() {
+int main(int argc, char *argv[]) {
 //    printf("%d\n", my_strlen("123456789"));
 //    char c[10] = "12";
 //    my_strcat(c, "");
@@ -88,42 +89,73 @@ int main() {
 //    printf("%s\n", get_chapter_file_name("1 234    ", "ty", "k"));
     setbuf(stdout, NULL);
     char c;
-    char name[100] = {0};
+    char *name = argv[1];
+    printf("name: %s\n", name);
 
-    printf("Type book name: ");
-    scanf("%99s", name);
+    char *output_prefix = argv[2];
+    printf("output prefix: %s\n", output_prefix);
+    char *output_suffix = ".txt";
+    if (argc > 3) {
+        output_suffix = argv[3];
+    }
+    printf("output suffix: %s\n", output_suffix);
 
-    char output_prefix[100] = {0};
-    printf("Type output prefix: ");
-    scanf("%99s", output_prefix);
-
-    char output_suffix[100] = {'.', 't', 'x', 't'};
-    printf("Type output suffix: ");
-    scanf("%99s", output_suffix);
-
-
-    char *file_name = get_chapter_file_name("PREFIX", output_prefix, output_suffix);
+    char *file_name = get_chapter_file_name("PREFACE", output_prefix, output_suffix);
     printf("%s\n", file_name);
     FILE *f_in = fopen(name, "r"), *f_out = fopen(file_name, "w");
     char *buf = NULL;
     size_t buf_size = 0;
 
-    if(!f_in || !f_out) {
+    if (!f_in || !f_out) {
         printf("Failed...");
         return -1;
     }
 
-    while(getline(&buf, &buf_size, f_in) != EOF) {
-        if(starts_with(buf, "CHAPTER")) {
+    unsigned int current_lines_count = 0;
+    unsigned int current_words_count = 0;
+    unsigned int current_char_count = 0;
+
+    unsigned int total_lines_count = 0;
+    unsigned int total_words_count = 0;
+    unsigned int total_char_count = 0;
+
+    while (getline(&buf, &buf_size, f_in) != EOF) {
+        if (starts_with(buf, "CHAPTER")) {
+            print_file_info(file_name, current_lines_count, current_words_count, current_char_count);
+            total_lines_count += current_lines_count;
+            total_words_count += current_words_count;
+            total_char_count += current_char_count;
+            current_lines_count = 1;
+            current_words_count = num_words(buf);
+            current_char_count = my_strlen(buf);
             file_name = get_chapter_file_name(buf, output_prefix, output_suffix);
             f_out = fopen(file_name, "w");
             continue;
         }
+        current_lines_count++;
+        current_words_count += num_words(buf);
+        current_char_count += my_strlen(buf);
         fprintf(f_out, "%s", buf);
     }
+    print_file_info(file_name, current_lines_count, current_words_count, current_char_count);
+
+    total_lines_count += current_lines_count;
+    total_words_count += current_words_count;
+    total_char_count += current_char_count;
     fclose(f_in);
     fclose(f_out);
     free(buf);
 
+    print_file_info("Total", total_lines_count, total_words_count, total_char_count);
+
+    printf("\n");
+}
+
+void print_file_info(const char *name, unsigned int lines_count, unsigned int words_count, unsigned int char_count) {
+    printf("%-30s: ", name);
+    printf("%d linse, ", lines_count);
+    printf("%d words, ", words_count);
+    printf("%d characters", char_count);
+    printf("\n");
 }
 
