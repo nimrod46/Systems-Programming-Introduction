9cb3894fc5bc2323b1ad93e1b776504d8a54def3
nimrod46 <nimrodma99@gmail.com>
2022-05-23 20:13:24 +0300

    A part

--- HW05/dump_hex.c
+++ HW05/dump_hex.c
@@ -6,11 +6,25 @@
 //
 // Created by nimrod on 23-May-22.
 //
+void print_hex(const char *block) {
+    long c = strtol(block, 0, 16);
+    if (c == strtol("0", 0, 16)) {
+        //printf("\n");
+        return;
+    }
+    if (c == strtol("0a", 0, 16)) {
+        printf("\n");
+        return;
+    }
+    printf("%c", c);
+}
+
+
 bool nextBlock(const char src[], char dst[], unsigned int offset, unsigned int *dst_size) {
     unsigned int i = offset;
     unsigned int maxLen = strlen(src);
     unsigned int dest_l = strlen(dst);
-    if(maxLen <= i) {
+    if (maxLen <= i) {
         return false;
     }
     for (int j = 0; j < dest_l; ++j) {
@@ -22,7 +36,7 @@ bool nextBlock(const char src[], char dst[], unsigned int offset, unsigned int *
     }
     dst[i - offset] = src[i];
 
-    unsigned int t = (i - offset) +1;
+    unsigned int t = (i - offset) + 1;
     *dst_size = t;
     return true;
 }
@@ -42,10 +56,10 @@ int main(int argc, char *argv[]) {
         //printf("%s\n", buff);
         while (nextBlock(buff, nextHex, i, &dst_size)) {
             i += dst_size;
-            printf("%c", strtol(nextHex, 0, 16));
+            print_hex(nextHex);
             nextHex[3] = '\0';
             nextHex[2] = '\0';
-            printf("%c", strtol(nextHex, 0, 16));
+            print_hex(nextHex);
         }
         //printf("\n");
     }
