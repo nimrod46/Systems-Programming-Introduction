8932ce8bdf4ca23ea93b2e6e7fc67382b9cf1522
nimrod46 <nimrodma99@gmail.com>
2022-05-24 13:36:44 +0300

    Fixed part A

---
+++ HW5/dump_hex.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <string.h>
+#include <stdlib.h>
+
+//
+// Created by nimrod on 23-May-22.
+//
+
+
+
+void print_hex(int c) {
+    printf("%02x", c);
+}
+
+int main(int argc, char *argv[]) {
+
+    FILE *input = fopen(argv[1], "rb");
+    int currentBit = 0;
+    printf("%07x", currentBit);
+    bool firstBit = true;
+    int prev;
+    int c;
+    while ((c = fgetc(input)) != EOF) {
+        if (firstBit) {
+            firstBit = !firstBit;
+            printf(" ");
+            prev = c;
+            continue;
+        }
+        firstBit = !firstBit;
+        print_hex(c);
+        print_hex(prev);
+        currentBit += 2;
+        if (currentBit % 16 == 0) {
+            printf("\n");
+            printf("%07x", currentBit);
+        }
+    }
+    fclose(input);
+    if(!firstBit) {
+        currentBit++;
+        print_hex('\0');
+        print_hex(prev);
+    }
+    printf("\n");
+    printf("%07x", currentBit);
+}
